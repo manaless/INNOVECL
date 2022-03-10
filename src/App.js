7cb0ce62a01864d5cdd0a0e8b0ced107923c3cf5
ImaneT123 <101304152+ImaneT123@users.noreply.github.com>
2022-03-10 03:14:46 +0100

    Update App.js
    Rectification de la modification précédente commit erroné

--- src/App.js
+++ src/App.js
@@ -1,49 +1,17 @@
-import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
-import {Menu} from 'element-react';
+import logo from './logo.svg';
+import './App.css';
+import NavBar from './Components/NavBar';
+import Footer from './Components/Footer';
+import CarouselTest from './Components/Carousel';
 
-import 'element-theme-default';
-import { BrowserRouter as Router, Route, NavLink,Routes } from "react-router-dom";
-import CarouselTest from './Carousel';
-import Etape1 from './Etape1';
-import Questions from './Questions';
-import Divergence from './Divergence';
-import Notation from './Notation';
 
-import { createContext } from 'react';
-
-const AppContext = createContext();
-
-export class NavBar extends Component {
-  onSelect() {
-
-  }
-
-            render(){
-            return (
-              <Router>
-              <div>
-                <Menu theme="dark" defaultActive="1" className="el-menu-demo" mode="horizontal" onSelect={this.onSelect.bind(this)}>
-                  <Menu.Item index="1"><NavLink style={{ textDecoration: 'none' }} to="/">InnovEcl</NavLink></Menu.Item>
-                  <Menu.Item index="2"><NavLink style={{ textDecoration: 'none' }}  to="/Questions">Critères</NavLink></Menu.Item>
-                  <Menu.Item index="3"><NavLink style={{ textDecoration: 'none' }} to="/Questions">Questions</NavLink></Menu.Item>
-                  <Menu.Item index="4"><NavLink style={{ textDecoration: 'none' }}  to="/">Divergence</NavLink></Menu.Item>
-                  <Menu.Item index="5"><NavLink style={{ textDecoration: 'none' }}  to="/">Convergence</NavLink></Menu.Item>
-                </Menu>
-                <div className="line"></div>
-                <Routes>
-                <Route exact path='/' element={<CarouselTest/>}/>
-                <Route exact path='/Questions' element={<Questions/>}/>                
-                </Routes>
-                
-              </div>
-              </Router>
-            );}
-          
-          
+function App() {
+  return (
+    <div className="App">
+      <NavBar/>
+      <Footer/>
+    </div>
+  );
 }
-    
 
- export default NavBar;
-
-
+export default App;
